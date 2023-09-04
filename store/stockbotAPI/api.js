bb9abd8a06eb5c54b9612bbd43be54b92ff5f78f
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-09-04 23:13:48 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/stockbotAPI/api.js
+++ store/stockbotAPI/api.js
@@ -1,104 +1,112 @@
-import axios from "axios"
+import axios from "axios";
 const stockbotAPI = axios.create({
   baseURL: "https://stockbot-42909.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return stockbotAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return stockbotAPI.post(`/api/v1/login/`, payload.data)
+  return stockbotAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return stockbotAPI.post(`/api/v1/signup/`, payload.data)
+  return stockbotAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function modules_two_factor_authentication_2fa_retrieve(payload) {
-  return stockbotAPI.get(`/modules/two-factor-authentication/2fa`)
+  return stockbotAPI.get(`/modules/two-factor-authentication/2fa`);
 }
+
 function modules_two_factor_authentication_2fa_create(payload) {
-  return stockbotAPI.post(`/modules/two-factor-authentication/2fa`)
+  return stockbotAPI.post(`/modules/two-factor-authentication/2fa`);
 }
+
 function modules_two_factor_authentication_twofactorauth_list(payload) {
-  return stockbotAPI.get(`/modules/two-factor-authentication/twofactorauth/`)
+  return stockbotAPI.get(`/modules/two-factor-authentication/twofactorauth/`);
 }
+
 function modules_two_factor_authentication_twofactorauth_create(payload) {
-  return stockbotAPI.post(
-    `/modules/two-factor-authentication/twofactorauth/`,
-    payload.data
-  )
+  return stockbotAPI.post(`/modules/two-factor-authentication/twofactorauth/`, payload.data);
 }
+
 function modules_two_factor_authentication_twofactorauth_retrieve(payload) {
-  return stockbotAPI.get(
-    `/modules/two-factor-authentication/twofactorauth/${payload.id}/`
-  )
+  return stockbotAPI.get(`/modules/two-factor-authentication/twofactorauth/${payload.id}/`);
 }
+
 function modules_two_factor_authentication_twofactorauth_update(payload) {
-  return stockbotAPI.put(
-    `/modules/two-factor-authentication/twofactorauth/${payload.id}/`,
-    payload.data
-  )
+  return stockbotAPI.put(`/modules/two-factor-authentication/twofactorauth/${payload.id}/`, payload.data);
 }
-function modules_two_factor_authentication_twofactorauth_partial_update(
-  payload
-) {
-  return stockbotAPI.patch(
-    `/modules/two-factor-authentication/twofactorauth/${payload.id}/`,
-    payload.data
-  )
+
+function modules_two_factor_authentication_twofactorauth_partial_update(payload) {
+  return stockbotAPI.patch(`/modules/two-factor-authentication/twofactorauth/${payload.id}/`, payload.data);
 }
+
 function modules_two_factor_authentication_twofactorauth_destroy(payload) {
-  return stockbotAPI.delete(
-    `/modules/two-factor-authentication/twofactorauth/${payload.id}/`
-  )
+  return stockbotAPI.delete(`/modules/two-factor-authentication/twofactorauth/${payload.id}/`);
 }
-function modules_two_factor_authentication_twofactorauth_send_otp_create(
-  payload
-) {
-  return stockbotAPI.post(
-    `/modules/two-factor-authentication/twofactorauth/send_otp/`,
-    payload.data
-  )
+
+function modules_two_factor_authentication_twofactorauth_send_otp_create(payload) {
+  return stockbotAPI.post(`/modules/two-factor-authentication/twofactorauth/send_otp/`, payload.data);
 }
+
 function modules_two_factor_authentication_verify_destroy(payload) {
-  return stockbotAPI.delete(
-    `/modules/two-factor-authentication/verify/${payload.id}/`
-  )
+  return stockbotAPI.delete(`/modules/two-factor-authentication/verify/${payload.id}/`);
 }
+
 function rest_auth_login_create(payload) {
-  return stockbotAPI.post(`/rest-auth/login/`, payload.data)
+  return stockbotAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return stockbotAPI.get(`/rest-auth/logout/`)
+  return stockbotAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return stockbotAPI.post(`/rest-auth/logout/`)
+  return stockbotAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return stockbotAPI.post(`/rest-auth/password/change/`, payload.data)
+  return stockbotAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return stockbotAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return stockbotAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return stockbotAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return stockbotAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_create(payload) {
-  return stockbotAPI.post(`/rest-auth/registration/`, payload.data)
+  return stockbotAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return stockbotAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
+  return stockbotAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return stockbotAPI.get(`/rest-auth/user/`)
+  return stockbotAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return stockbotAPI.put(`/rest-auth/user/`, payload.data)
+  return stockbotAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return stockbotAPI.patch(`/rest-auth/user/`, payload.data)
+  return stockbotAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -124,4 +132,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
