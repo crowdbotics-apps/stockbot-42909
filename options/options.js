24d68f816f92ce82529b7847ec8b4b5e747b63d4
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-09-04 22:50:10 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "stockbot-42909",
   url: "https://stockbot-42909.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/ScreenAI2/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, View, Text, Image, StatusBar } from 'react-native';
+
+const SplashScreen = () => {
+  return <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <View style={styles.content}>
+        <Image style={styles.logo} source={{
+        uri: 'https://tinyurl.com/42evm3m3'
+      }} />
+        <Text style={styles.title}>Welcome to Our App</Text>
+        <Text style={styles.subtitle}>We're preparing things for you</Text>
+      </View>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  content: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 20
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    textAlign: 'center',
+    marginBottom: 10
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'center'
+  }
+});
+export default SplashScreen;
